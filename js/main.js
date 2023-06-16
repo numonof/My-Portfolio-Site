71c188efc85f4c9126b9bf5549ad07ebb91d6d87
numonof <islomxonnumonov3701@gmail.com>
2023-06-16 10:47:10 +0500

    add book blog btn

--- js/main.js
+++ js/main.js
@@ -57,3 +57,30 @@ for ( i = 0; i < acardion.length; i++) {
         this.classList.toggle('active')
     });
 }
+
+// Modal
+let showModal = document.querySelector('.open_modal');
+let modal = document.querySelector('.modal');
+let closeModalBtn = document.querySelector('.close-modal');
+let owerlay = document.querySelector('.modal_owerlay');
+// Open Modal
+const openModal = function(){
+    modal.classList.remove('hidden');
+    owerlay.classList.remove('hidden');
+}
+
+showModal.addEventListener('click', openModal)
+// closeModal
+const closeModal = function(){
+    modal.classList.add('hidden');
+    owerlay.classList.add('hidden');
+}
+closeModalBtn.addEventListener( 'click', closeModal);
+owerlay.addEventListener( 'click', closeModal);
+
+document.addEventListener( 'keydown' , function (evt) {
+    if (evt.key === 'Escape'){
+        closeModal()  
+    }
+})
+closeModal()
